8a61b6e94253f51e0215d9258f01601e8daed258
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-04-01 11:46:38 +1300

    Add navbar

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from './Navbar'
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>App</h1>
       <p>React development has begun!</p>
     </div>
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <ul className="navbar">
+      <div className="navbar-items">
+        <li className="navbar-item">Home</li>
+        <li className="navbar-item">About</li>
+        <li className="navbar-item">Add</li>
+      </div>
+    </ul>
+  )
+}
