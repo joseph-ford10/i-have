58896f4e9053b8c1c4d348aff9fc0020aa325e9d
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-04-01 18:58:28 +1300

    Add buttons for reset and new selection

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import { setConstantValue } from 'typescript'
 
 export default function Card(props) {
-  const [activity, setActivity] = useState({})
-
   const { activities } = require('../../activities')
   const newArr = []
   for (let i = 0; i < activities.length; i++) {
@@ -14,22 +12,37 @@ export default function Card(props) {
       newArr.push(activities[i])
     }
   }
+  const [activity, setActivity] = useState(
+    newArr[Math.floor(Math.random() * newArr.length)]
+  )
 
-  useEffect(() => {
-    setActivity(newArr[Math.floor(Math.random() * newArr.length)])
-  }, [newArr])
+  function handleClick(event) {
+    event.target.className == 'generateNew'
+      ? setActivity(newArr[Math.floor(Math.random() * newArr.length)])
+      : props.pageChange('form')
+    console.log(event)
+    console.log(activity)
+  }
 
   return (
-    <div>
-      <h1>
-        Something you can {props.selection.type} in {props.selection.time}{' '}
-        minutes? Why not try...
-      </h1>
-      <h2>{activity.name}</h2>
-      <h3>{activity.description}</h3>
-      <p>Estimated time: {activity.timeEst}</p>
-      <p>Link: {activity.link}</p>
-      <p>Other users give it: {activity.score}</p>
-    </div>
+    <>
+      <div>
+        <h1>
+          Something you can {props.selection.type} in {props.selection.time}{' '}
+          minutes? Why not try...
+        </h1>
+        <h2>{activity.name}</h2>
+        <h3>{activity.description}</h3>
+        <p>Estimated time: {activity.timeEst}</p>
+        <p>Link: {activity.link}</p>
+        <p>Other users give it: {activity.score}</p>
+      </div>
+      <button onClick={handleClick} className="generateNew">
+        Don't like it? Generate another suggestion
+      </button>
+      <button onClick={handleClick} className="returnToSelect">
+        Return to selection
+      </button>
+    </>
   )
 }
