ae655e3f2c73bf4bfd3bb2593ba583b76ff58b4b
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-04-02 19:22:46 +1200

    Increase font size for about page

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -4,18 +4,18 @@ export default function About() {
       <div className="card">
         <div className="card-container">
           <h1>About</h1>
-          <p>
+          <p className="about-text">
             Got some time to spare? Use{' '}
             <span className="product-name">I've Got</span> to get customised
             recommendations for activities based on how much time you've got and
             what you feel like doing.{' '}
           </p>
-          <p>
+          <p className="about-text">
             Got an activity that you think would be perfect for{' '}
             <span className="product-name">I've Got</span>? Go to the "Add" page
             to contribute.
           </p>
-          <p>
+          <p className="about-text">
             Got questions, comments, or other queries about{' '}
             <span className="product-name">I've Got</span>? Contact us at
             loremipsum@####.com
