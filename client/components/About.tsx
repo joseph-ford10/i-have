f623973d9a5aa4e3535e7374506873a2225c06f6
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-04-02 14:47:58 +1200

    Create 'About' page

---
+++ client/components/About.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <>
+      <div className="card">
+        <div className="card-container">
+          <h1>About</h1>
+          <p>
+            Got some time to spare? Use{' '}
+            <span className="product-name">I've Got</span> to get customised
+            recommendations for activities based on how much time you've got and
+            what you feel like doing.{' '}
+          </p>
+          <p>
+            Got an activity that you think would be perfect for{' '}
+            <span className="product-name">I've Got</span>? Go to the "Add" page
+            to contribute.
+          </p>
+          <p>
+            Got questions, comments, or other queries about{' '}
+            <span className="product-name">I've Got</span>? Contact us at
+            loremipsum@####.com
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './Navbar'
 import Home from './Home'
+import About from './About'
 
 function App() {
   return (
